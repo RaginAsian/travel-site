a86e0b1a9964e5cde16b438291afdd95a1c370ee
Jerry <jaultz@gmail.com>
2017-08-06 23:32:15 -0400

    middle of BEM lesson

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
 	console.log("Awsome! This works.");
@@ -12,10 +17,26 @@ gulp.task('html', function(){
 
 });
 
-gulp.task('watch' function() {
+gulp.task('styles', function(){
 
-	watch('./app/index.html', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars,nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+
+});
+
+
+gulp.task('watch', function() {
+
+	watch('./app/index.html', function(){
+
 		gulp.start('html');
+
+	});
+
+	watch("./app/assets/styles/**/*.css", function(){
+
+		gulp.start("styles");''
 
 	});
 
